277677d4dc740273597990825ef27c5e860d087a
Tyson Matanich <tymat@matanich.com>
2012-11-06 15:58:09 -0800

    Adding v1

---
+++ README.md
@@ -0,0 +1,23 @@
+﻿# GetViewportWidth
+
+Allows you to get the width of the viewport using JavaScript.
+
+* Author: Tyson Matanich
+* License: MIT
+
+## Size and delivery
+
+Currently, `getViewportWidth.js` compresses to around 541 bytes (~0.5 KB), after minify and gzip. To minify, you might try these online tools: [Microsoft Ajax Minifier]:(http://ajaxmin.codeplex.com/), [Uglify]:(http://marijnhaverbeke.nl/uglifyjs), [Yahoo Compressor]:(http://refresh-sf.com/yui/), or [Closure Compiler](http://closure-compiler.appspot.com/home). Serve with gzip compression.
+
+## Sample
+
+
+```html
+<script type="text/javascript">
+	var viewportWidth = window.getViewportWidth();
+</script>
+```
+
+## Support
+
+GetViewportWidth supports a broad range of browsers and devices (there are currently no known unsupported browsers).
---
+++ getViewportWidth.js
@@ -0,0 +1,43 @@
+﻿/*! GetViewportWidth | Author: Tyson Matanich, 2012 | License: MIT */
+(function (window) {
+	window.getViewportWidth = function () {
+		var width;
+		var document = window.document;
+		var documentElement = document.documentElement;
+		if (window.innerWidth === undefined) {
+			// IE6 & IE7 don't have window.innerWidth
+			width = documentElement.clientWidth;
+		}
+		else if (window.innerWidth > documentElement.clientWidth) {
+			// WebKit doesn't include scrollbars while calculating viewport width so we have to get fancy
+
+			// Insert markup to test if a media query will match document.doumentElement.clientWidth
+			var bodyElement = document.createElement("body");
+			bodyElement.id = "vpw-test-b";
+			bodyElement.style.cssText = "overflow:scroll";
+			var divElement = document.createElement("div");
+			divElement.id = "vpw-test-d";
+			divElement.style.cssText = "position:absolute;top:-1000px";
+			// Getting specific on the CSS selector so it won't get overridden easily
+			divElement.innerHTML = "<style>@media(width:" + documentElement.clientWidth + "px){body#vpw-test-b div#vpw-test-d{width:7px!important}}</style>";
+			bodyElement.appendChild(divElement);
+			documentElement.insertBefore(bodyElement, document.head);
+
+			if (divElement.offsetWidth == 7) {
+				// Media query matches document.documentElement.clientWidth
+				width = documentElement.clientWidth;
+			}
+			else {
+				// Media query didn't match, use window.innerWidth
+				width = window.innerWidth;
+			}
+			// Cleanup
+			documentElement.removeChild(bodyElement);
+		}
+		else {
+			// Default to use window.innerWidth
+			width = window.innerWidth;
+		}
+		return width;
+	};
+})(this);
